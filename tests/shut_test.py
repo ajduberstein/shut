80475c8e7269f0795a7193d1e1706fb656be9af1
Andrew Duberstein <duberstein@uber.com>
2015-12-27 12:42:09 -0800

    Update test cases

--- tests/shut_test.py
+++ tests/shut_test.py
@@ -14,27 +14,31 @@ import StringIO
 
 from mock import patch
 
-from shut import shape_unix_time
+from shut import UnixTimeParser
 
 
 class TestShut(unittest.TestCase):
 
+    def setUp(self):
+        self.utp = UnixTimeParser('2015-01-01', '2015-10-01', 'UTC')
+        self.utp_pacific = UnixTimeParser('2015-01-01', '2015-10-01', 'US/Pacific')
+
     def test_shape_unix_time_unchanged_input(self):
         with patch('sys.stdout', new=StringIO.StringIO()) as fakeOutput:
             sys.stdin = StringIO.StringIO('asdlkj')
-            shape_unix_time('2015-01-01', '2015-10-01', 'UTC')
+            self.utp.shape_unix_time()
             assert fakeOutput.getvalue().strip() == 'asdlkj'
 
     def test_shape_unix_time_converted(self):
         with patch('sys.stdout', new=StringIO.StringIO()) as fakeOutput:
             sys.stdin = StringIO.StringIO('asdlkj,1443304641,more_text,1443304642')
-            shape_unix_time('2015-01-01', '2015-10-01', 'US/Pacific')
+            self.utp_pacific.shape_unix_time()
             assert fakeOutput.getvalue().strip() == 'asdlkj,2015-09-26 14:57:21,more_text,2015-09-26 14:57:22'
 
     def test_shape_unix_time_converted_utc(self):
         with patch('sys.stdout', new=StringIO.StringIO()) as fakeOutput:
             sys.stdin = StringIO.StringIO('asdlkj,1443304641,more_text,1443304642')
-            shape_unix_time('2015-01-01', '2015-10-01', 'UTC')
+            self.utp.shape_unix_time()
             assert fakeOutput.getvalue().strip() == 'asdlkj,2015-09-26 21:57:21,more_text,2015-09-26 21:57:22'
 
 if __name__ == '__main__':
