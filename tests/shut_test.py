81fdef25ac92c825e17b63b338468b1b0b43fca9
Andrew Duberstein <duberstein@uber.com>
2015-12-28 09:04:42 -0400

    Complete test cases

--- tests/shut_test.py
+++ tests/shut_test.py
@@ -8,6 +8,7 @@ test_shut
 Tests for `shut` module.
 """
 
+import subprocess
 import sys
 import unittest
 import StringIO
@@ -42,9 +43,29 @@ class TestShut(unittest.TestCase):
             assert fakeOutput.getvalue().strip() == 'asdlkj,2015-09-26 21:57:21,more_text,2015-09-26 21:57:22'
 
     def test_command_line_execution(self):
-        # TODO test command line execution
-        # Popen('cat ./README.md | ./shut/shut.py')
-        pass
+        proc = subprocess.check_output(
+            'echo \'{"ts": 1440999387, "msg": ""}\' | ./shut/shut.py',
+            shell=True)
+        assert '2015-08-31 05:36:27' in proc
+
+    def test_command_line_execution_min(self):
+        proc = subprocess.check_output(
+            'echo \'{"ts": 584928000, "msg": ""}\' | ./shut/shut.py -m 1988-01-01',
+            shell=True)
+        assert '1988-07-15 00:00:00' in proc
+
+    def test_command_line_execution_min_max(self):
+        proc = subprocess.check_output(
+            'echo \'{"ts": 584928000, "msg": "$1435086362.00"}\' | ./shut/shut.py -m 1988-01-01 -M 2013-02-14',
+            shell=True)
+        assert '1988-07-15 00:00:00' in proc and '$1435086362.00' in proc
+
+    def test_command_line_execution_min_tz(self):
+        proc = subprocess.check_output(
+            'echo \'{"ts": 584928000, "msg": "$1435086362.00"}\' | ./shut/shut.py -m 1988-01-01 -t US\/Pacific',
+            shell=True)
+        print proc
+        assert '1988-07-14 17:00:00' in proc
 
 if __name__ == '__main__':
     TestShut()
